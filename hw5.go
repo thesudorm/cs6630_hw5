0f18ee2e0769180a1e2634d211927d0aedaf7152
Joseph Bauer <root@thesudorm.com>
2021-11-27 10:08:56 -0500

    Using minimum support on first generations.

--- hw5.go
+++ hw5.go
@@ -12,12 +12,17 @@ func main() {
     t := readTransactions()
     //minSup := 0.20
     dict := map[string]int {}
+    f1 := GenerateF1(t)
 
-    for i := 0; i < len(t); i++ {
-        record := t[i]
-        for j := 0; j < len(record); j++ {
-            item := record[j]
-            if len(item) > 0 {
+    //for _, test := range f1 {
+    //fmt.Print(test, " ")
+    //}
+    //fmt.Println()
+
+    for _, item := range f1 {
+        for _, trans := range t {
+            _, found := Find(trans, item)
+            if found {
                 if _, ok := dict[item]; ok {
                     dict[item] += 1
                 } else {
@@ -51,4 +56,32 @@ func readTransactions() [][]string {
     }
 
     return t
+}
+
+// Find unique items
+func GenerateF1(t [][]string) []string {
+    f1 := []string{}
+    for i := 0; i < len(t); i++ {
+        record := t[i]
+        for j := 0; j < len(record); j++ {
+            item := record[j]
+            if len(item) > 0 {
+                _, found := Find(f1, item)
+                if !found {
+                    f1 = append(f1, item)
+                }
+            }
+        }
+    }
+
+    return f1
+}
+
+func Find(slice []string, val string) (int, bool) {
+    for i, item := range slice {
+        if item == val {
+            return i, true
+        }
+    }
+    return -1, false
 }
