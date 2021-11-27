410a0f9fa2bdcfb61c3f79bdc2f05b6885f29e85
Joseph Bauer <root@thesudorm.com>
2021-11-26 20:48:05 -0500

    Counting frequency of single items.

--- hw5.go
+++ hw5.go
@@ -9,8 +9,33 @@ import (
 )
 
 func main() {
+    t := readTransactions()
+    //minSup := 0.20
+    dict := map[string]int {}
+
+    for i := 0; i < len(t); i++ {
+        record := t[i]
+        for j := 0; j < len(record); j++ {
+            item := record[j]
+            if len(item) > 0 {
+                if _, ok := dict[item]; ok {
+                    dict[item] += 1
+                } else {
+                    dict[item] = 0
+                }
+            }
+        }
+    }
+
+    for k, v := range dict {
+        fmt.Println(k, v)
+    }
+}
+
+func readTransactions() [][]string {
     csv_file, _ := os.Open("retail_dataset.csv")
     r := csv.NewReader(csv_file)
+    t := [][]string{}
 
     for {
         record, err := r.Read()
@@ -22,6 +47,8 @@ func main() {
             log.Fatal(err)
         }
 
-        fmt.Println(record)
+        t = append(t, record)
     }
+
+    return t
 }
