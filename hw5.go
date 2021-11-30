908545d552a5321e919a8ecab7c50309efb6e8bc
Joseph Bauer <root@thesudorm.com>
2021-11-29 19:11:08 -0500

    Able to count transactions with multiple items.

--- hw5.go
+++ hw5.go
@@ -2,48 +2,55 @@ package main
 
 import (
     "fmt"
+    "strings"
     "sort"
     "encoding/csv"
     "io"
     "log"
     "os"
 )
+/*
+/*  TODO:*/
+/*      1. Prune candidates in CandidateGen
+/*      2. Fix CandidateGen to work with k > 2
+*/
 
 func main() {
     transactions := readTransactions()
     f1 := GenerateF1(transactions, 0.50)
     fk := f1
-    count := 0
-    //numOfItemsInTrans := len(transactions[0])
+    dict := map[string]int{}
 
-    //fmt.Println(CandidateGen(fk, 2))
+    ck := CandidateGen(fk, 2);
 
-    //for k := 2; len(fk) > 0; k++ {
-    for k := 2; k <= 2; k++ { // forcing one iteration for now
-        ck := CandidateGen(fk, k) // this needs to make the next generation of candidates
-        fmt.Println(ck)
-        count = 0
-        // I think if I change everything to be a string,
-        // that it will be easier to count with a dict
+    for _, c := range ck {
         for _, t := range transactions {
-            candidatesInTrans := true
-            for _, c := range ck {
-                for _, ci := range c {
-                    _, found := Find(t, ci)
-                    if found == false {
-                        candidatesInTrans = false
-                    }
-                }
+            itemsetInTransaction := true
+            //itemInTransaction := false
+            itemset := strings.Split(c, " ")
 
-                if candidatesInTrans {
-                    count += 1
+            for _, item := range itemset {
+                _, found := Find(t, item)
+                if found == false {
+                    itemsetInTransaction = false
                 }
             }
 
+            if itemsetInTransaction {
+                if _, ok := dict[c]; ok {
+                    dict[c] += 1
+                } else {
+                    dict[c] = 1
+                }
+            }
         }
     }
 
-    fmt.Println(fk)
+    //fmt.Println(fk)
+
+    for k, v := range dict {
+        fmt.Println(k, v)
+    }
 }
 
 func readTransactions() [][]string {
@@ -68,9 +75,9 @@ func readTransactions() [][]string {
 }
 
 // Find unique items
-func GenerateF1(t [][]string, supp float64) [][]string {
+func GenerateF1(t [][]string, supp float64) []string {
     f       := []string{}
-    f1      := [][]string{}
+    f1      := []string{}
     dict    := map[string]int {}
 
     for i := 0; i < len(t); i++ {
@@ -103,7 +110,7 @@ func GenerateF1(t [][]string, supp float64) [][]string {
 
     for _, item := range f {
         if float64(dict[item]) / float64(len(t)) >= supp {
-            f1 = append(f1, []string {item})
+            f1 = append(f1, item)
         }
     }
 
@@ -119,19 +126,21 @@ func Find(slice []string, val string) (int, bool) {
     return -1, false
 }
 
-func CandidateGen(fk [][]string, k int) [][]string {
-    toReturn := [][]string{}
-    ck := [][]string{}
+func CandidateGen(fk []string, k int) []string {
+    ck := []string{}
 
+    // Generate a super set of candidates
     for i := 0; i < len(fk); i++ {
         for j := 0; j < len(fk); j++ {
-            _, found := Find(fk[i], fk[j][0])
-            if found == false {
-                ck = append(ck, append(fk[i], fk[j][0]))
+            if fk[i] != fk[j] {
+                combined := fk[i] + " " + fk[j]
+                ck = append(ck, combined)
             }
         }
     }
 
-    toReturn = ck
-    return toReturn
+    // Prune candidates
+    // TODO
+
+    return ck
 }
